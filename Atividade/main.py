fd802a03dbad7d5b6c7e0921cc0a8fca7ab4c593
gemouraa <160125708+gemouraa@users.noreply.github.com>
2024-09-30 12:47:29 +0000

    update

---
+++ Atividade/main.py
@@ -0,0 +1,10 @@
+import os
+
+from Atividade.arquivo_1 import  Endereco
+from Atividade.Arquivo_2 import Funcionario
+from Atividade.engenherio import Engenheiro
+from Atividade.medico import Medico
+
+os.system("clear")
+
+pessoa_1 = Funcionario("Guilherme", "1111111", "Guilherme@gmail.com", Endereco("Anisio"))
