39ec54bb82d7a83d51413299b34969ddd8c99e9c
gemouraa <160125708+gemouraa@users.noreply.github.com>
2024-09-30 12:36:36 +0000

    Atividade

--- Atividade/Arquivo_2.py
+++ Atividade/Arquivo_2.py
@@ -1,4 +1,5 @@
 from abc import ABC, abstractmethod 
+from Atividade.arquivo_1 import Endereco
 
 class Funcionario (ABC): 
     def __init__(self, nome: str, telefone: str ,email: str, endereco: Endereco) -> None:
--- Atividade/arquivo_1.py
+++ Atividade/arquivo_1.py
@@ -1,1 +1,20 @@
-print("Colaborador 1.")
+import os
+
+class Endereco:
+    def __init__(self,logadouro: str,numero: str,complemento: str,cep: str,cidade:str) -> None:
+        self.logadouro = logadouro
+        self.numero = numero
+        self.complemento = complemento
+        self.cep = cep
+        self.cidade = cidade
+
+    def __str__(self) -> str:
+        return (f"\nLogadouro: {self.logadouro}"
+                f"\nNúmero: {self.numero}"
+                f"\nComplemento: {self.complemento}"
+                f"\nCep: {self.cep}"
+                f"\nCidade: {self.cidade}")
+
+ 
+
+
