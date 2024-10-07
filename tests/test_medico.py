f055907bfe0454a71662a2c77789ea7033e59ebf
Ciroula <159453767+luiz4a@users.noreply.github.com>
2024-10-07 13:50:02 +0000

    atividade

--- tests/test_medico.py
+++ tests/test_medico.py
@@ -7,11 +7,11 @@ os.system("cls||clear")
 
 @pytest.fixture
 def pessoa_valida():
-        medico = Medico("Guilherme", "12541254", "@gmail.com", "123456789", 4000, Endereco("Anisio", 44, "Perto da padaria", 40715366, "Salvador" ))
+        medico = Medico("moura", "12541254", "@gmail.com", "123456789", 4000, Endereco("Anisio", 44, "Perto da padaria", 40715366, "Salvador" ))
         return medico
 
 def test_medico_nome_valido(pessoa_valida):
-     assert pessoa_valida.nome == "Guilherme"
+     assert pessoa_valida.nome == "moura"
 
 def test_medico_telefone_valido(pessoa_valida):
      assert pessoa_valida.telefone == "12541254"
