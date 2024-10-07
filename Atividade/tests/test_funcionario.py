7403accb04b7a048f274219fb9e857c1a208fffc
Ciroula <159453767+luiz4a@users.noreply.github.com>
2024-10-07 11:16:30 +0000

    update

---
+++ Atividade/tests/test_funcionario.py
@@ -0,0 +1,11 @@
+import pytest
+
+from Atividade.funcionario import Funcionario
+
+@pytest.fixture
+def funcionario_valido():
+    funcionario = funcionario("Moura",111,"guilherme@gmail.com")
+    return funcionario
+def test_funcionario_nome_valido(funcionario_valido):
+    funcionario_valido.nome = "Moura"
+    assert funcionario_valido.nome == "Moura"
